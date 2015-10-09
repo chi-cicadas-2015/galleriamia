cdb2f288e52b7e8a593b23d2c852052118d6d96f
pdgonzalez872 <pdgonzalez872@gmail.com>
2015-10-09 10:31:03 -0500

    Create Event model and migration

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -3,5 +3,4 @@ class Friend < ActiveRecord::Base
   belongs_to :user
   belongs_to :friend, class_name: User
 
-  has_secure_password
 end
---
+++ db/migrate/20151009152347_create_events.rb
@@ -0,0 +1,17 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+
+      t.datetime :date
+      t.datetime :start_time
+      t.datetime :end_time
+      t.string :event_url
+      t.string :event_address
+      t.string :event_city
+      t.string :event_zip
+      t.string :host
+
+      t.timestamps null: false
+    end
+  end
+end
