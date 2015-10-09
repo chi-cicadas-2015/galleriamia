f40b51588abbf0c8d9d4b27e06ae84f9f881d5b5
pdgonzalez872 <pdgonzalez872@gmail.com>
2015-10-08 21:00:40 -0500

    Create nested routes.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.create!(name: "paulo", email: "gonzalez", password: "testing1234”)
