67913252cedeaaa9ef881de4e1630c1f4cc77d02
pdgonzalez872 <pdgonzalez872@gmail.com>
2015-10-09 15:07:20 -0500

    Working on user and followers syntax

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
 
   has_many :friends
-  has_many :friends, through: :friends
+  has_many :followers, through: :friends, source: "User"
+
   has_many :event_users
   has_many :events, through: :event_users
+
   has_many :collections
 
   has_secure_password
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,60 +1,37 @@
-User.create(name: "paulo",
-            email:"p@g.com",
-            password:"testing1234",
-            statement: "These are tests",
-            avatar_url: "https://upload.wikimedia.org/wikipedia/commons/c/c4/Lleyn_sheep.jpg",
-            photo_url: "http://www.glaucus.org.uk/Sheep0007.jpg",
-            type_of_user: "artist"
-            )
+user_types = ["artist", "friend"]
 
-User.create(name: "David",
-            email:"p@g.com",
-            password:"testing1234",
-            statement: "These are tests",
-            avatar_url: "https://upload.wikimedia.org/wikipedia/commons/c/c4/Lleyn_sheep.jpg",
-            photo_url: "http://www.glaucus.org.uk/Sheep0007.jpg",
-            type_of_user: "friend"
-            )
+# Create users
+20.times do
 
-User.create(name: "Greggo",
-            email:"g@g.com",
-            password:"testing1234",
-            statement: "These are tests",
-            avatar_url: "https://upload.wikimedia.org/wikipedia/commons/c/c4/Lleyn_sheep.jpg",
-            photo_url: "http://www.glaucus.org.uk/Sheep0007.jpg",
-            type_of_user: "artist"
-            )
+  u = User.create(name: Faker::Name.name,
+                  email:Faker::Internet.email,
+                  password:"testing1234",
+                  statement: Faker::Company.catch_phrase,
+                  avatar_url: "https://upload.wikimedia.org/wikipedia/commons/c/c4/Lleyn_sheep.jpg",
+                  photo_url: "http://www.glaucus.org.uk/Sheep0007.jpg",
+                  type_of_user: user_types.sample)
+end
 
-User.create(name: "Nic",
-            email:"n@g.com",
-            password:"testing1234",
-            statement: "These are tests",
-            avatar_url: "https://upload.wikimedia.org/wikipedia/commons/c/c4/Lleyn_sheep.jpg",
-            photo_url: "http://www.glaucus.org.uk/Sheep0007.jpg",
-            type_of_user: "artist"
-            )
+# Make friends
+20 times do
+  Friend.create(user_id: rand(User.count), friend_id: rand(User.count))
+end
 
-Event.create(start_time: '10-09-2015 17:00',
-             end_time: '10-09-2015 19:00',
-             event_url: 'google.com',
-             event_address: 'google.com',
-             event_city: 'chicago',
-             event_zip: '60614',
-             host: 'DBC')
-
- Event.create(start_time: '11-11-2015 17:00',
-              end_time: '11-11-2015 19:00',
-              event_url: 'google.com',
-              event_address: 'google.com',
-              event_city: 'chicago',
-              event_zip: '60614',
-              host: 'DBC')
-
-Friend.create(user_id: 1, friend_id: 2)
-Friend.create(user_id: 1, friend_id: 3)
-Friend.create(user_id: 3, friend_id: 1)
 
 Collection.create(user_id: 1, name: "Collection test 1", description: "This is the description")
+
+# Generate Pieces
+
+
+
+
+
+# Generate Collections, assign pieces to Collections
+
+
+
+
+
 
 Piece.create(title: "Sheep Stanza1", size: "L", medium: "A medium", image_url_thumb: "www", url_full: "www_full", description: "This is a painting about a sheep, yes, you got it")
 Piece.create(title: "Sheep Stanza2", size: "L", medium: "A medium", image_url_thumb: "www", url_full: "www_full", description: "This is a painting about a sheep, yes, you got it")
