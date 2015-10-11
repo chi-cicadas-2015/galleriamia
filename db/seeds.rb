af6898d8cefe9d518bcd439c56943ef225399fa6
pdgonzalez872 <pdgonzalez872@gmail.com>
2015-10-10 20:49:18 -0500

    Finish seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ user_types = ["artist", "friend"]
   user.save
 end
 
-# create 80 friends
+# Create 80 friends
 80.times do
   friend = User.new(name: Faker::Name.name,
                   email:Faker::Internet.email,
@@ -25,10 +25,38 @@ end
   friend.save
 end
 
-# create random friendships - user_types.sample
+# Create random friendships
 50.times do
   target = User.find(rand(1..User.count))
   follower = User.find(rand(1..User.count))
 
   target.followers << follower if target.id != follower.id
+end
+
+# Create tags
+40.times do
+  c = Collection.new(user_id: rand(1..User.count),
+                     name: Faker::Company.catch_phrase,
+                     description: Faker::Company.catch_phrase)
+  c.save
+end
+
+30.times do
+  t = Tag.new(name: Faker::Commerce.product_name)
+  t.save
+end
+
+sizes = ["9x12", "10x14", "15x22", "22x30"]
+
+artist_pool = User.where(type_of_user: "artist")
+
+100.times do
+  p = Piece.new(title: Faker::Commerce.product_name,
+                size: sizes.sample,
+                medium: "medium",
+                image_url_thumb: "url",
+                url_full: "url",
+                description: Faker::Company.catch_phrase,
+                artist_id: artist_pool.sample.id)
+  p.save
 end
