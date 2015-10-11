368dbe21e5300695e3e19b34f7776f70b875ba5e
Nic Lombardi <email": null>
2015-10-11 15:35:09 -0500

    implemented background image filter

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -1,4 +1,8 @@
 $( document ).ready(function(){
     $(".button-collapse").sideNav();
-    $('.slider').slider({full_width: true, indicators: false});
+    $('.slider').slider({
+      full_width: true,
+      indicators: false,
+      transition: 1000,
+    });
 })
