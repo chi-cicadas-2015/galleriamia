b2daa5c815aa67b49e1e2ebf26f45ac49a032dee
pdgonzalez872 <pdgonzalez872@gmail.com>
2015-10-12 09:58:53 -0500

    continue working on feature tests

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -3,20 +3,20 @@ require 'rails_helper'
 
 feature "User - Landing page" do
   scenario "User visits the root and is greated with options" do
-    visit '/'
+    visit "/"
     expect(page).to have_text("Galleria Mia")
     expect(page).to have_text("Random Artist")
     expect(page).to have_text("Sign Up")
   end
 
   scenario "User selects the Sign Up option and creates an account" do
-    visit "/users/new"
+    visit new_user_path
     fill_in "Name", :with => "Rafael"
     fill_in "Email", :with => "rafael@fakemail.com"
     fill_in "Password", :with => "testing1234"
     click_button "Save User"
     expect(page).to have_text("Your account was saved successfully")
-    expect(page).to have_text("John Doe")
+    expect(page).to have_content("Rafael")
   end
 
 
@@ -30,19 +30,25 @@ feature "User - Profile page" do
                    statement: "This is a test statement",
                    artist: true)
     user = User.last
-    visit "/users/#{user.id}"
+    visit user_path(user)
 
-    expect(page).to have_text("#{user.name}")
-    expect(page).to have_text("Edit Settings")
-    expect(page).to have_text("Collections")
-    expect(page).to have_text("About")
-    expect(page).to have_text("Events")
+    expect(page).to have_content("#{user.name}")
+    expect(page).to have_content("Edit Settings")
+    expect(page).to have_content("Collections")
+    expect(page).to have_content("About")
+    expect(page).to have_content("Events")
   end
 end
 
 
-feature "" do
-  scenario "" do
+feature "User - Edit settings page" do
+  scenario "User clicks the Edit settings button from his/her profile page" do
+    user =  User.last
+    visit edit_user_path(user)
 
+
+
+    expect(page).to have_text("Name")
+    
   end
 end
