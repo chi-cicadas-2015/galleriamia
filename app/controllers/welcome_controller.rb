86b803e6160f70b6634d6abb9e3eb09c20e62766
Madelyn Kasula <madelynkasula@gmail.com>
2015-10-12 16:12:11 -0500

    Implement search functionality for artists and collections

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,9 +6,8 @@ class WelcomeController < ApplicationController
   def search
     @query = params[:query]
     @artist_results = User.where("(artist = ?) AND (lower(name) LIKE ?)", true, "%#{@query.downcase}%")
-    @collection_results
-    @piece_results
-    @tag_results
+    @collection_results = Collection.where("(lower(name) LIKE ? OR lower(description) LIKE ?)", "%#{@query.downcase}%", "%#{@query.downcase}%")
+    @tag_results = Tag.where("lower(name) LIKE ?", "%#{@query.downcase}%")
   end
 
 end
