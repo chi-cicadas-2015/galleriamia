006b5b63fe2ace4a14aaf7fde02258c5c84d91a0
pdgonzalez872 <pdgonzalez872@gmail.com>
2015-10-12 16:29:15 -0500

    Finish profile migration, implement headshot from paperclip

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,8 +3,7 @@ class Profile < ActiveRecord::Base
   belongs_to :artist, class_name: User
 
   has_attached_file :headshot,
-                    :styles => { :thumb => "100x100>",
-                                 :large => "300x300>"}
+                    :styles => { :thumb => "100x100>"}
   validates_attachment_content_type :headshot, :content_type => /\Aimage\/.*\Z/
   validates_attachment_file_name :headshot, matches: [/png\Z/, /PNG\Z/, /jpe?g\Z/, /JPE?G\Z/]
 end
