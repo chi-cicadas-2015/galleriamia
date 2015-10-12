4ab71f8b7ba63bc844ac9672026c6f6d7d9ba010
Madelyn Kasula <madelynkasula@gmail.com>
2015-10-12 12:23:26 -0500

    Work in progress - search functionality, need to fuzzy search results

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,11 @@ class WelcomeController < ApplicationController
   end
 
   def search
-
+    @query = params[:query]
+    @artist_results = User.where(["artist='%s' AND name='%s'", true, @query])
+    @collection_results
+    @piece_results
+    @tag_results
   end
 
 end
