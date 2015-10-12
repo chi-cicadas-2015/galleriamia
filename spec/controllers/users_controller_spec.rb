0f7d2f002b04ef926d3a42bc23565273c733cb02
pdgonzalez872 <pdgonzalez872@gmail.com>
2015-10-11 19:39:54 -0500

    Work with controller testing and feature testing. Work in progress.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,7 +1,24 @@
-# require 'rails_helper'
-#
-# describe UsersController, :type => :request do
-#   it "GET index" do
-#     get :index
-#   end
-# end
+require 'rails_helper'
+
+describe UsersController do
+
+  let!(:user) {User.create!(name: "Michelangelo",
+                    email: "michelangelo@mail.com",
+                    password:"testing1234",
+                    statement: "This is the test statement",
+                    artist: true)}
+  describe "GET index" do
+    it "assigns all users as @artists" do
+      get :index
+      # expect(assigns(:artist)).to eq([user])
+    end
+  end
+
+  describe "GET #show" do
+    it "assigns user as the @user" do
+      get :show, { id: user.to_param }
+      # expect(assigns(:user)).to eq(game)
+    end
+  end
+
+end
---
+++ spec/features/user_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+
+feature "User features" do
+  scenario "visit route" do
+    visit '/'
+
+  end
+end
