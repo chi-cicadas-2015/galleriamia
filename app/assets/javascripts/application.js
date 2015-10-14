d6d00929a6743922d483c1d982bd1cac6fae607c
Madelyn Kasula <madelynkasula@gmail.com>
2015-10-14 15:05:00 -0500

    Map event results using Google Maps

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,51 @@
 //= require materialize-sprockets
 //= require jquery_ujs
 //= require_tree .
+
+$(document).ready(function() {
+  function initialize() {
+    var allElements = document.getElementsByClassName('location');
+    var allIds = getIds(allElements);
+    var allLatlng = getLatlngs(allIds);
+    var mapCanvas = document.getElementById('map');
+
+    var mapOptions = {
+      zoom: 8,
+      center: allLatlng[0],
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+
+    var markers = [];
+    for (i = 0; i < allLatlng.length; i++) {
+      var marker = new google.maps.Marker({
+        position: allLatlng[i],
+        title: "Hello World!"
+      });
+      marker.setMap(map);
+    };
+  };
+
+  function getIds(elements) {
+    var allIds = [];
+    for (i = 0, n = elements.length; i < n; ++i) {
+      var el = elements[i];
+      if (el.id) { allIds.push(el.id); }
+    };
+    return allIds;
+  };
+
+  function getLatlngs(ids) {
+    var latLngs = [];
+    for (i = 0; i < ids.length; i++) {
+      var lat = Number(ids[i].split(",")[0]);
+      var lon = Number(ids[i].split(",")[1]);
+      var myLatlng = new google.maps.LatLng(lat, lon);
+      latLngs.push(myLatlng);
+    };
+    return latLngs;
+  };
+
+  google.maps.event.addDomListener(window, 'load', initialize);
+});
