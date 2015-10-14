1adcb024ba08b13ce906ed65113d3ffdc3bff2f1
pdgonzalez872 <pdgonzalez872@gmail.com>
2015-10-14 17:54:16 -0500

    Start on events model tests

---
+++ spec/models/event_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe Event do
+
+  
+
+end
+
+
+# create_table "events", force: :cascade do |t|
+#   t.datetime "start_time"
+#   t.datetime "end_time"
+#   t.string   "event_url"
+#   t.string   "event_address"
+#   t.string   "event_city"
+#   t.string   "event_zip"
+#   t.string   "host"
+#   t.integer  "user_id"
+#   t.datetime "created_at",    null: false
+#   t.datetime "updated_at",    null: false
+# end
