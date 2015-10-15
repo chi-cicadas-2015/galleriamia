7b8609b57bf90d24195896170332ea662a19beaa
Madelyn Kasula <madelynkasula@gmail.com>
2015-10-15 17:08:35 -0500

    Follow functionality works with AJAX

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -13,12 +13,35 @@ $( document ).ready(function(){
 
   $('.follow').on('click', function(e) {
     e.preventDefault();
-    $(this).html('<i class="unfollow secondary-content medium material-icons icon_color">favorite</i>');
+    var that = $(this);
+    var artistId = $('.col-xs-4').attr('id');
+    var artistName = $('.col-xs-4 ').html();
+
+    $.ajax({
+      url: "/users/" + artistId + "/friends/new"
+    })
+    .done(function(response) {
+      $('#info_friends').append('<h5 id="friend_link"><a href="/users/" + artistId>' + artistName + '</a></h5><br>')
+      that.html('<i class="unfollow secondary-content medium material-icons icon_color">favorite</i>');
+    });
+
   });
 
   $('.unfollow').on('click', function(e) {
     e.preventDefault();
-    $(this).html('<i class="follow secondary-content medium material-icons icon_color">favorite_border</i>');
+    var that = $(this);
+    var artistId = $('.col-xs-4').attr('id');
+    var followerId = $('.user').attr('id');
+    var artistName = $('.col-xs-4 ').html();
+
+    $.ajax({
+      url: "/users/" + artistId + "/friends/" + followerId + "/edit"
+    })
+    .done(function(response) {
+      $('#info_friends').remove('<h5 id="friend_link"><a href="/users/" + artistId>' + artistName + '</a></h5><br>')
+      that.html('<i class="follow secondary-content medium material-icons icon_color">favorite_border</i>');
+    });
+
   });
 
   showUserCollections();
