3b0e5de0440db5203dd7e276d152f31c028f8300
pdgonzalez872 <pdgonzalez872@gmail.com>
2015-10-15 10:22:35 -0500

    Add tests and refactor code

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,25 +1,7 @@
 require 'rails_helper'
 
-feature "User - Landing page" do
-  scenario "User visits the root and is greated with options" do
-    visit "/"
-    expect(page).to have_link("Galleria Mia")
-    expect(page).to have_link("Random Artist")
-    expect(page).to have_link("Sign Up")
-  end
+feature "User features" do
 
-  scenario "User selects the Sign Up option and creates an account" do
-    visit new_user_path
-    fill_in "Name", :with => "Rafael"
-    fill_in "Email", :with => "rafael@fakemail.com"
-    fill_in "Password", :with => "testing1234"
-    click_button "Save User"
-    expect(page).to have_text("Your account was saved successfully")
-    expect(page).to have_content("Rafael")
-  end
-end
-
-feature "User - Edit settings page" do
   let(:artist) { User.create!(name: "Pablo Picasso",
                     email: "testing@mail.com",
                     password:"testing1234",
@@ -32,9 +14,57 @@ feature "User - Edit settings page" do
                                    website_url: "https://en.wikipedia.org/wiki/Vincent_van_Gogh",
                                    primary_medium: "Oil",
                                    headshot: File.new("#{Rails.root}/public/imgs/favicon63x54.png")) }
-  scenario "User clicks the Edit settings button from his/her profile page" do
-    # user =  User.create
-    visit edit_user_path(artist)
-    expect(page).to have_text("Name")
+
+  # New is used in order to not persist the artist. This is done to simulate the login feature.
+  let(:not_artist) { User.new(name: "Pablo NOT Picasso",
+                     email: "friend@mail.com",
+                     password:"testing1234",
+                     statement: "This is the test statement",
+                     avatar: File.new("#{Rails.root}/public/imgs/favicon63x54.png")) }
+
+  feature "User - Landing page" do
+    scenario "User visits the root and is greated with options" do
+      visit "/"
+      expect(page).to have_link("Galleria Mia")
+      expect(page).to have_link("Random Artist")
+      expect(page).to have_link("Sign Up")
+    end
+
+    scenario "User selects the Sign Up option and creates an account" do
+      visit new_user_path
+      fill_in "Name", :with => not_artist.name
+      fill_in "Email", :with => not_artist.email
+      fill_in "Password", :with => not_artist.password
+      click_button "Save User"
+      expect(page).to have_text("Your account was saved successfully")
+      expect(page).to have_content(not_artist.name)
+    end
+
+  end
+
+  feature "User - Edit functionality" do
+
+    context "User logs in" do
+
+      def user_logs_in
+        visit '/login'
+        fill_in "Email", :with => artist.email
+        fill_in "Password", :with => artist.password
+        click_button("Save Session")
+      end
+
+      it "User clicks the 'Edit' button in the About tab" do
+        user_logs_in
+        visit user_path(artist.id)
+        click_link('About')
+        click_link('Edit Profile')
+        expect(page).to render_template(:edit)
+      end
+
+      it "User can see what information to edit" do
+        visit edit_user_path(artist)
+        expect(page).to have_text("Name")
+      end
+    end
   end
 end
