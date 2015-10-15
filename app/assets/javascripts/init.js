e91c466d571299e8f9332d0bf96f61826e2d0fe6
Madelyn Kasula <madelynkasula@gmail.com>
2015-10-15 17:25:36 -0500

    Fix AJAX on unfollowing artists

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -11,38 +11,37 @@ $( document ).ready(function(){
     transition: 1000,
   });
 
-  $('.follow').on('click', function(e) {
-    e.preventDefault();
-    var that = $(this);
-    var artistId = $('.col-xs-4').attr('id');
-    var artistName = $('.col-xs-4 ').html();
+    $('.follow').on('click', function(e) {
+      e.preventDefault();
+      var that = $(this);
+      var artistId = $('.col-xs-4').attr('id');
+      var artistName = $('.col-xs-4 ').html();
 
-    $.ajax({
-      url: "/users/" + artistId + "/friends/new"
-    })
-    .done(function(response) {
-      $('#info_friends').append('<h5 id="friend_link"><a href="/users/" + artistId>' + artistName + '</a></h5><br>')
-      that.html('<i class="unfollow secondary-content medium material-icons icon_color">favorite</i>');
+      $.ajax({
+        url: "/users/" + artistId + "/friends/new"
+      })
+      .done(function() {
+        $('#info_friends').append('<h5 id="friend_link"><a href="/users/" + artistId>' + artistName + '</a></h5><br>')
+        that.html('<i class="unfollow secondary-content medium material-icons icon_color">favorite</i>');
+      });
     });
 
-  });
+    $('.unfollow').on('click', function(e) {
+      e.preventDefault();
+      var that = $(this);
+      var artistId = $('.col-xs-4').attr('id');
+      var followerId = $('.user').attr('id');
+      var artistName = $('.col-xs-4 ').html();
 
-  $('.unfollow').on('click', function(e) {
-    e.preventDefault();
-    var that = $(this);
-    var artistId = $('.col-xs-4').attr('id');
-    var followerId = $('.user').attr('id');
-    var artistName = $('.col-xs-4 ').html();
+      $.ajax({
+        dataType: "html",
+        url: "/users/" + artistId + "/friends/" + followerId + "/edit"
+      })
+      .done(function() {
+        that.html('<i class="follow secondary-content medium material-icons icon_color">favorite_border</i>');
+      });
 
-    $.ajax({
-      url: "/users/" + artistId + "/friends/" + followerId + "/edit"
-    })
-    .done(function(response) {
-      $('#info_friends').remove('<h5 id="friend_link"><a href="/users/" + artistId>' + artistName + '</a></h5><br>')
-      that.html('<i class="follow secondary-content medium material-icons icon_color">favorite_border</i>');
     });
-
-  });
 
   showUserCollections();
 
