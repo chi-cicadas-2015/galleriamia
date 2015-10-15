f31986f99f75790c54d4e01bc2fe2d2f410f115f
Madelyn Kasula <madelynkasula@gmail.com>
2015-10-15 10:51:49 -0500

    Add jQuery to change follow/unfollow without reload - add AJAX

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -11,6 +11,16 @@ $( document ).ready(function(){
     transition: 1000,
   });
 
+  $('.follow').on('click', function(e) {
+    e.preventDefault();
+    $(this).html('<i class="unfollow secondary-content medium material-icons icon_color">favorite</i>');
+  });
+
+  $('.unfollow').on('click', function(e) {
+    e.preventDefault();
+    $(this).html('<i class="follow secondary-content medium material-icons icon_color">favorite_border</i>');
+  });
+
   showUserCollections();
 
   googleMaps();
